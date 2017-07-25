495ee71d2b64d4fea2924ca1ca43528a6135f6e7
Teodor B <teomati@gmail.com>
2017-07-25 14:24:55 +0200

    initial

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/teo-mateo/gosm/statemachine"
+)
+
+const (
+	State1 = "STATE1"
+	State2 = "STATE2"
+	State3 = "STATE3"
+)
+
+const (
+	e1 = "e1"
+	e2 = "e2"
+	e3 = "e3"
+	e4 = "e4"
+)
+
+func main() {
+	test1()
+}
+
+func test1() {
+	fmt.Println("Hello statemachine")
+
+	sm := statemachine.NewStateMachine()
+	fmt.Printf("Main() %p\n", sm)
+
+	sm.Configure().
+		AddStates(State1, State2, State3).
+		AddTransition(State1, State2, e1).
+		AddTransition(State2, State3, e2).
+		AddTransition(State3, State2, e1).
+		AddTransition(State3, State1, e3).
+		AddTransition(State1, State3, e4).
+		OnEnter(State2, func(state string, payload interface{}) {
+			fmt.Println("Entering", state)
+		}).
+		OnEnter(State3, func(state string, payload interface{}) {
+			fmt.Println("Entering", state)
+		})
+
+	fmt.Println(sm)
+	sm.Trigger(e1, nil)
+	sm.Trigger(e2, nil)
+	fmt.Println(sm)
+}
---
+++ statemachine/statemachine.go
@@ -0,0 +1,204 @@
+package statemachine
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+)
+
+/*
+type transition struct {
+	event   string
+	toState string
+}
+*/
+
+type StateMachine struct {
+	smc                      *StateMachineConfig
+	states                   []string
+	transitions              map[string]map[string]string
+	currentState             string
+	panicOnIllegalTransition bool
+	onEnters                 map[string]func(string, interface{})
+	onExits                  map[string]func(string, interface{})
+}
+
+func NewStateMachine() *StateMachine {
+	sm := StateMachine{
+		states:      make([]string, 0),
+		transitions: make(map[string]map[string]string),
+		onEnters:    make(map[string]func(string, interface{})),
+		onExits:     make(map[string]func(string, interface{})),
+	}
+	smc := NewStateMachineConfig(&sm)
+	sm.smc = &smc
+
+	return &sm
+}
+
+func (sm *StateMachine) containsState(state string) bool {
+	contains := false
+	for _, b := range sm.states {
+		if b == state {
+			contains = true
+			break
+		}
+	}
+	return contains
+}
+
+func (sm *StateMachine) String() string {
+	buf := bytes.Buffer{}
+	buf.WriteString(fmt.Sprintf("StateMachine representation\n"))
+	for _, state := range sm.states {
+		format := "\t%s\n"
+		if sm.GetCurrentState() == state {
+			format = "\t*%s*\n"
+		}
+		buf.WriteString(fmt.Sprintf(format, state))
+		for e, s := range sm.transitions[state] {
+			buf.WriteString(fmt.Sprintf("\t\t%s -> %s\n", e, s))
+		}
+	}
+	return string(buf.Bytes())
+}
+
+func (sm *StateMachine) Configure() *StateMachineConfig {
+	return sm.smc
+}
+
+func (sm *StateMachine) GetCurrentState() string {
+	if sm.currentState == "" {
+		panic(errors.New("state machine not initialized"))
+	}
+	return sm.currentState
+}
+
+func (sm *StateMachine) Trigger(event string, payload interface{}) {
+	if event == "" {
+		panic(errors.New("cannot trigger with empty event"))
+	}
+
+	allowedTransitions := sm.transitions[sm.currentState]
+	nextState, ok := allowedTransitions[event]
+	if !ok && sm.panicOnIllegalTransition {
+		panic(errors.New(fmt.Sprintf("attempted illegal transition: %s + %s", sm.currentState, event)))
+	}
+
+	//fire exit event
+	onExit, ok := sm.onExits[sm.currentState]
+	if ok {
+		onExit(sm.currentState, payload)
+	}
+
+	sm.currentState = nextState
+
+	//fire enter event
+	onEnter, ok := sm.onEnters[sm.currentState]
+	if ok {
+		onEnter(sm.currentState, payload)
+	}
+}
+
+type StateMachineConfig struct {
+	sm *StateMachine
+}
+
+func NewStateMachineConfig(sm *StateMachine) StateMachineConfig {
+	return StateMachineConfig{sm: sm}
+}
+
+func (smc *StateMachineConfig) AddState(state string) *StateMachineConfig {
+	if state == "" {
+		panic(errors.New("non-empty argument expected: state"))
+	}
+
+	if smc.sm.containsState(state) {
+		panic(errors.New(fmt.Sprintf("state machine already contains state: %s", state)))
+	}
+
+	smc.sm.states = append(smc.sm.states, state)
+
+	//set current state if not set
+	if smc.sm.currentState == "" {
+		smc.sm.currentState = state
+	}
+
+	return smc
+}
+
+func (smc *StateMachineConfig) AddStates(states ...string) *StateMachineConfig {
+	if len(states) == 0 {
+		log.Fatal("non-empty argument expected: states")
+	}
+
+	for _, state := range states {
+		smc.AddState(state)
+	}
+
+	//set current state if not set
+	if smc.sm.currentState == "" {
+		smc.sm.currentState = states[0]
+	}
+
+	return smc
+}
+
+func (smc *StateMachineConfig) SetInitialState(state string) *StateMachineConfig {
+	if state == "" {
+		log.Fatal("non-empty argument expected: state")
+	}
+
+	if !smc.sm.containsState(state) {
+		log.Fatal("state machine does not contain state: ", state)
+	}
+
+	smc.sm.currentState = state
+	return smc
+}
+
+func (smc *StateMachineConfig) AddTransition(fromState string, toState string, event string) *StateMachineConfig {
+
+	if !smc.sm.containsState(fromState) {
+		panic(errors.New(fmt.Sprintf("unknown state: %s", fromState)))
+	}
+
+	if !smc.sm.containsState(toState) {
+		panic(errors.New(fmt.Sprintf("unknown state: %s", toState)))
+	}
+
+	//get transition slice for the "from" state
+	tmap := smc.sm.transitions[fromState]
+	//make if null
+	if tmap == nil {
+		tmap = make(map[string]string)
+
+	}
+	//append
+	tmap[event] = toState
+
+	//add
+	smc.sm.transitions[fromState] = tmap
+	return smc
+}
+
+func (smc *StateMachineConfig) OnEnter(state string, f func(string, interface{})) *StateMachineConfig {
+	if !smc.sm.containsState(state) {
+		panic(errors.New(fmt.Sprintf("unknown state: %s", state)))
+	}
+	smc.sm.onEnters[state] = f
+	return smc
+}
+
+func (smc *StateMachineConfig) OnExit(state string, f func(string, interface{})) *StateMachineConfig {
+	if !smc.sm.containsState(state) {
+		panic(errors.New(fmt.Sprintf("unknown state: %s", state)))
+	}
+	smc.sm.onExits[state] = f
+	return smc
+}
+
+func (smc *StateMachineConfig) PanicOnIllegalTransition(truefalse bool) {
+	smc.sm.panicOnIllegalTransition = truefalse
+}
